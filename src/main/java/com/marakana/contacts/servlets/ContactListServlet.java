4efb8be5bc7a39b2170f590efbb61679073a664c
SampathCA <sampathamarakoon@gmail.com>
2015-02-07 18:49:28 +0530

    Implemented findAll()

--- src/main/java/com/marakana/contacts/servlets/ContactListServlet.java
+++ src/main/java/com/marakana/contacts/servlets/ContactListServlet.java
@@ -1,7 +1,6 @@
 package com.marakana.contacts.servlets;
 
 import java.io.IOException;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
